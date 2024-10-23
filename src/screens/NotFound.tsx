9be7ee866cb145e0a2ef120dc336552fc084059b
Cristian Prochnow <cristianprochnow@gmail.com>
2024-10-22 22:04:46 -0300

    feat: add initial structure for NotFound

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/screens/NotFound.css';
 
 export function NotFound() {
@@ -7,8 +8,13 @@ export function NotFound() {
         <img src="public/not-found.svg" alt="Not Found Hero" />
       </header>
 
-      <h1>Ooops...</h1>
-      <p>Nada encontrado aqui.</p>
+      <div>
+        <h1>Ooops...</h1>
+        <p>
+          Infelizmente nada foi encontrado aqui. O que acha
+          de <Link to="/">voltar ao início</Link> e tentar novamente?
+        </p>
+      </div>
     </main>
   );
 }
