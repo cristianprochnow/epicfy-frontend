9d55f21232fcf93b4c26947d5562f80179072c50
Cristian Prochnow <cristianprochnow@gmail.com>
2024-11-25 01:06:02 -0300

    feat: add initial link to idea details

--- src/screens/Idea.tsx
+++ src/screens/Idea.tsx
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom';
+import '../styles/screens/Idea.css';
+
+type IdeaRouteParams = { id?: string };
+
 export function Idea() {
+  const params: IdeaRouteParams = useParams();
+
   return (
-    <main id="idea-screen"></main>
+    <main id="idea-screen">
+      Rota com o ID {params.id}
+    </main>
   );
 }
--- src/screens/Ideas.tsx
+++ src/screens/Ideas.tsx
@@ -1,9 +1,10 @@
 import { ArrowLeft, ArrowRight, ThumbsDown, ThumbsUp } from 'react-feather';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from '../components/Button';
 import '../styles/screens/Ideas.css';
 
 export function Ideas() {
+  const navigate = useNavigate();
   const location = useLocation();
   const search = new URLSearchParams(location.search).get('search');
 
@@ -12,6 +13,10 @@ export function Ideas() {
   function onPreviousPageHandler() {}
 
   function onNextPageHandler() {}
+
+  function onIdeaDetailsHandler(id: number) {
+    navigate(`/ideas/${id}`);
+  }
 
   return (
     <main id="ideas-screen">
@@ -29,107 +34,7 @@ export function Ideas() {
         </header>
 
         <div>
-          <div>
-            <div className='identification'>
-              <p>Adicionar mais emojis ao WhatsApp</p>
-              <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
-            </div>
-
-            <footer>
-              <div className="status">
-                <span>Em progresso</span>
-              </div>
-
-              <div className='votes'>
-                <span title='Like'>
-                  <ThumbsUp />
-                  <span>50</span>
-                </span>
-                <span title='Dislike'>
-                  <ThumbsDown />
-                  <span>10</span>
-                </span>
-              </div>
-
-            </footer>
-          </div>
-
-          <div>
-            <div className='identification'>
-              <p>Adicionar mais emojis ao WhatsApp</p>
-              <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
-            </div>
-
-            <footer>
-              <div className="status">
-                <span>Em progresso</span>
-              </div>
-
-              <div className='votes'>
-                <span title='Like'>
-                  <ThumbsUp />
-                  <span>50</span>
-                </span>
-                <span title='Dislike'>
-                  <ThumbsDown />
-                  <span>10</span>
-                </span>
-              </div>
-
-            </footer>
-          </div>
-
-          <div>
-            <div className='identification'>
-              <p>Adicionar mais emojis ao WhatsApp</p>
-              <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
-            </div>
-
-            <footer>
-              <div className="status">
-                <span>Em progresso</span>
-              </div>
-
-              <div className='votes'>
-                <span title='Like'>
-                  <ThumbsUp />
-                  <span>50</span>
-                </span>
-                <span title='Dislike'>
-                  <ThumbsDown />
-                  <span>10</span>
-                </span>
-              </div>
-
-            </footer>
-          </div>
-
-          <div>
-            <div className='identification'>
-              <p>Adicionar mais emojis ao WhatsApp</p>
-              <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
-            </div>
-
-            <footer>
-              <div className="status">
-                <span>Em progresso</span>
-              </div>
-
-              <div className='votes'>
-                <span title='Like'>
-                  <ThumbsUp />
-                  <span>50</span>
-                </span>
-                <span title='Dislike'>
-                  <ThumbsDown />
-                  <span>10</span>
-                </span>
-              </div>
-
-            </footer>
-          </div>
-
-          <div>
+          <div onClick={() => onIdeaDetailsHandler(1)}>
             <div className='identification'>
               <p>Adicionar mais emojis ao WhatsApp</p>
               <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
