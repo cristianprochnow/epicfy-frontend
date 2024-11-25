3590fc1f3070ece3b63eb95e4a6127ab6c4a5e82
Cristian Prochnow <cristianprochnow@gmail.com>
2024-11-25 08:00:05 -0300

    feat: add idea details page

--- src/screens/Idea.tsx
+++ src/screens/Idea.tsx
@@ -1,4 +1,6 @@
+import { ArrowLeft, ThumbsDown, ThumbsUp } from 'react-feather';
 import { useParams } from 'react-router-dom';
+import { Button } from '../components/Button';
 import '../styles/screens/Idea.css';
 
 type IdeaRouteParams = { id?: string };
@@ -6,9 +8,41 @@ type IdeaRouteParams = { id?: string };
 export function Idea() {
   const params: IdeaRouteParams = useParams();
 
+  function onBackHandler() {}
+
   return (
     <main id="idea-screen">
-      Rota com o ID {params.id}
+      <header className="wrapper">
+        <Button onHandler={onBackHandler}>
+          <ArrowLeft /> Listagem
+        </Button>
+
+        <span>Epicfy</span>
+      </header>
+
+      <form className='wrapper'>
+        <h1>Adicionar mais emojis ao WhatsApp</h1>
+        <p>Uma ideia de aplicação de emojis ao WhatsApp, para aumentar a diversidade</p>
+
+        <footer>
+          <div className="status">
+            <span>Em progresso</span>
+          </div>
+
+          <div className='votes'>
+            <span title='Like'>
+              <ThumbsUp />
+              <span>50</span>
+            </span>
+            <span title='Dislike'>
+              <ThumbsDown />
+              <span>10</span>
+            </span>
+          </div>
+        </footer>
+      </form>
+
+      <section className='wrapper'></section>
     </main>
   );
 }
